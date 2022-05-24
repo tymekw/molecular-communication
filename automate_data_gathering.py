9a6a491f670fb9851dbfea99d12c819f27a4af80
Krzysztof Polak <krzysiek.polak04@gmail.com>
2022-05-24 20:03:55 +0200

    Simple automate running accord

--- automate_data_gathering.py
+++ automate_data_gathering.py
@@ -1,19 +1,26 @@
+import os
 import re
 import subprocess
+
+ACCORD_HOME = "C:\\Users\\P1\\Desktop\\AcCoRD-1.4.2"
+ACCORD_EXEC = ACCORD_HOME + "\\bin\\accord_win.exe"
 with open('template.txt', 'r') as file:
     template = file.readlines()
 
+modulation_strength = [25, 50, 75, 100, 125, 150]
 template_str = "".join(template)
-k = template_str.replace("$SEED$", str(current_seed))
 
-# for i in range(0, 10):
-#     command = ['./waf', '--run "--nWifiJammers={}"'.format(str(i))]
-#     th = []
-#     for _ in range(10):
-#         output = subprocess.check_output(command)
-#         result = re.findall(" .* Mbit/s", str(output))[0].split(" ")[-2]
-#         th.append(result)
-#     results[str(i)] = th
+for current_seed in range(1, 3):
+    template_str_withM_mod_level = template_str.replace("$MODULATION_BITS$", str(2))
+    template_with_seed = template_str_withM_mod_level.replace("$SEED$", str(current_seed))
+    for mod_str in modulation_strength:
+        print("dupa")
+        template_with_mod_str = template_with_seed.replace("$MODULATION_STR$", str(mod_str))
+        config_name = "automate_config_{}_{}.txt".format(str(2), str(mod_str))
+        with open(ACCORD_HOME + "\\config\\{}".format(config_name), 'w+') as file:
+            file.write(template_with_mod_str)
+        # for i in range(0, 10):
+        command = [ACCORD_EXEC, config_name]
+        output = subprocess.check_output(command, cwd=ACCORD_HOME + "\\bin", shell=True)
+        print("output: ", str(output))
 
-    with open("wyniki.txt", 'a+') as file:
-        file.writelines(str(results[str(i)]))
