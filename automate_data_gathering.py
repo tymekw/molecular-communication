9bf9c24dc2ddef57051643cea7e7135d93c7145d
tymekw <tymwid22@gmail.com>
2022-05-24 21:09:23 +0200

    fix bug

--- automate_data_gathering.py
+++ automate_data_gathering.py
@@ -20,7 +20,7 @@ sychro_map = {'1': [1, 0, 0, 1, 0, 0, 1, 0, 0],
 # BIT_SEQUENCE_LIST
 results = ''
 try:
-    ACCORD_HOME = "C:\\Users\\P1\\Desktop\\AcCoRD-1.4.2"
+    ACCORD_HOME = "D:\\AGH\\II STOPIEŃ\\es\\molecular"
     ACCORD_EXEC = ACCORD_HOME + "\\bin\\accord_win.exe"
     with open('template.txt', 'r') as file:
         template = file.readlines()
@@ -36,7 +36,7 @@ try:
             template_with_mod_str = template_with_seed.replace("$MODULATION_STR$", str(mod_str))
             for m_bit in modulation_bits:
                 template_str_withM_mod_level = template_with_mod_str.replace("$MODULATION_BITS$", str(m_bit))
-                automate_result_output_filename = "automate_results_SEED_{}_MOD_LVL_{}_MOD_STR_{}.txt".format(
+                automate_result_output_filename = "automate_results_SEED_{}_MOD_LVL_{}_MOD_STR_{}".format(
                     str(current_seed), str(m_bit), str(mod_str))
                 expected_bits = [random.randint(0, 1) for _ in range(256)]
                 bits = sychro_map[str(m_bit)] + expected_bits
@@ -50,12 +50,15 @@ try:
                 command = [ACCORD_EXEC, config_name]
                 output = subprocess.check_output(command, cwd=ACCORD_HOME + "\\bin", shell=True)
 
-                result_full_path = ACCORD_HOME + '\\bin\\result\\' + automate_result_output_filename
+                result_full_path = ACCORD_HOME + '\\bin\\result\\' + automate_result_output_filename + \
+                                   '_SEED{}.txt'.format(m_bit)
+
                 with open(result_full_path, 'r') as file:
                     data = ''.join(file.readlines())
 
                 x = re.findall(r'Count:\n\t\t\t\t(.*)\n\tPassiveActor 3:', data)
                 resulted_data = x[0]
+                print(resulted_data)
                 demodulator = Demodulator(raw_data=resulted_data,
                                           symbol_len=100,
                                           modulation_bits=m_bit,
@@ -66,9 +69,12 @@ try:
                 demodulated = demodulator.demodulate(decoded)
 
                 ber = calculate_ber([int(i) for i in list(demodulated)], expected_bits)
-                results += 'RESULTS_SEED_{}_MOD_LVL_{}_MOD_STR_{}\nBER: {}\n\n'.format(str(current_seed), str(m_bit), str(mod_str), str(ber))
+                results += 'RESULTS_SEED_{}_MOD_LVL_{}_MOD_STR_{}\nBER: {}\n\n'.format(str(current_seed), str(m_bit),
+                                                                                       str(mod_str), str(ber))
+                print(results)
                 with open('results.txt', 'w+') as file:
                     file.write(results)
-except:
+except Exception as e:
+    print(e)
     with open('results.txt', 'w+') as file:
         file.write(results)
